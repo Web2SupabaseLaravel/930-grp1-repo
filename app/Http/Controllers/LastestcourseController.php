de5088d85ed2c3f042995a3e6cc2b41db14b1c56
WalaaTurkman <s12323751@stu.najah.edu>
2025-05-23 12:20:35 +0300

    Update .md

--- app/Http/Controllers/LastestcourseController.php
+++ app/Http/Controllers/LastestcourseController.php
@@ -5,31 +5,14 @@ use App\Models\Course;
 use Illuminate\Http\Request;
 class LastestcourseController extends Controller
 {
-public function index(Request $request)
-    {
-        $category = $request->input('category');
 
-        if ($category) {
-            // إذا المستخدم اختار تصنيف
-            $courses = Course::where('category', $category)
-                             ->orderBy('id', 'desc') 
-                             ->get();
-        } else {
-            // إذا المستخدم ما اختار شيء
-            $courses = Course::orderBy('id', 'desc')->get();
-        }
-
-return view('LastestcourseController', compact('courses', 'category'));
-    }
-
-
-    public function apiIndex(Request $request)
+public function apiIndex(Request $request)
 {
     $category = $request->input('category');
 
     if ($category) {
         $courses = Course::where('category', $category)
-                         ->orderBy('id', 'desc') // أو orderBy('created_at', 'desc') لو موجود
+                         ->orderBy('id', 'desc')
                          ->get();
     } else {
         $courses = Course::orderBy('id', 'desc')->get();
