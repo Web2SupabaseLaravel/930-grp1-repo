578ad7d5685520a699a24d291d49ff2b554adc47
manarimad <eng.manar.imad@gmail.com>
2025-05-25 17:44:28 +0300

    last commit (check code)

--- myproject/app/Http/Controllers/CourseController.php
+++ myproject/app/Http/Controllers/CourseController.php
@@ -91,7 +91,7 @@ $course->save();
             'catagory' => 'required|string|max:100',
             'description' => 'required|string',
             'price' => 'required|numeric|min:0',
-            'instructor_id' => 'uuid|required|exists:users,user_id',
+            'instructor_id' => 'uuid|required|exists:instructors,user_id',
             'learning_objectives' => 'required|string',
         ]);
 
