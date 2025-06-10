9b7fe6de95a3977c2e7811064275e7f55efe3074
manarimad <eng.manar.imad@gmail.com>
2025-06-10 22:52:06 +0300

    editing..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import CourseList from './components/courses/CourseList';
-// import CourseForm from './components/courses/CourseForm';
-// import CourseDetails from './components/courses/CourseDetails'; 
-// import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CourseList from './components/courses/CourseList';
+import CourseForm from './components/courses/CourseForm';
+import CourseDetails from './components/courses/CourseDetails'; 
+import './App.css';
 
-// const App = () => {
-//     return (
-//         <Router>
-//     <Switch>
-//         <Route exact path="/" component={CourseList} />
-//         <Route path="/courses/create" component={CourseForm} />
-//         <Route path="/courses/:id" component={CourseDetails} />
-//       </Switch>
-//         </Router>
-//     );
-// }
+const App = () => {
+    return (
+        <Router>
+    <Switch>
+        <Route exact path="/" component={CourseList} />
+        <Route path="/courses/create" component={CourseForm} />
+        <Route path="/courses/:id" component={CourseDetails} />
+      </Switch>
+        </Router>
+    );
+}
 
-// export default App;
+export default App;
 
 // import React from 'react';
 //     import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -41,26 +41,26 @@
 
 //     export default App;
 
-import React from 'react';
-  import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-  import CourseList from './components/CourseList.jsx';
-  import CourseForm from './components/CourseForm.jsx';
-  import CourseDetails from './components/CourseDetails.jsx';
-  import './App.css';
+// import React from 'react';
+//   import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//   import CourseList from './components/CourseList.jsx';
+//   import CourseForm from './components/CourseForm.jsx';
+//   import CourseDetails from './components/CourseDetails.jsx';
+//   import './App.css';
 
-  const App = () => {
-    return (
-      <div>
-        <h1>Test Render</h1> {/* Temporary test */}
-        <Router>
-          <Switch>
-            <Route exact path="/" component={CourseList} />
-            <Route path="/courses/create" component={CourseForm} />
-            <Route path="/courses/:id" component={CourseDetails} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  };
+//   const App = () => {
+//     return (
+//       <div>
+//         <h1>Test Render</h1> {/* Temporary test */}
+//         <Router>
+//           <Switch>
+//             <Route exact path="/" component={CourseList} />
+//             <Route path="/courses/create" component={CourseForm} />
+//             <Route path="/courses/:id" component={CourseDetails} />
+//           </Switch>
+//         </Router>
+//       </div>
+//     );
+//   };
 
-  export default App;
+//   export default App;
